2059bdbd84a4187c7b2b9f469537b07965c43679
Bart Klaasse <BART_k@live.nl>
2019-07-03 15:05:15 +0200

    Les 5 Console log versies

--- Section 2/js_sandbox/app.js
+++ Section 2/js_sandbox/app.js
@@ -1,1 +1,41 @@
+// Single line Comment
+
+/* 
+Multi line comment
+*/
+
+function ConsoleLogFunctions(){
+// Alert
 alert('test');
+
+// Console clearen
+console.clear();
+
+//Console log string
+console.log('test');
+//Console log ints
+console.log(123);
+
+// console log variabelen
+var greeting = 'hoi';
+console.log(greeting);
+//console log array
+console.log([1,2,3]);
+
+//console log object
+console.log({a:1,b:2});
+
+// Console log object as table
+console.table({a:1, b:2});
+
+// console log an error
+console.error('Dit is een error yo!');
+
+// console log an warning
+console.warn('dit is een warning yo!');
+
+// console timer
+console.time('hello')
+console.log('Een manier om te timen hoelang een script ergens over doet')
+console.timeEnd('hello');
+}
