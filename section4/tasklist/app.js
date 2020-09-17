d9d3d368f3dd0145b9b78972f876ffa974dfde03
Bart Klaasse <bart@bartklaasse.nl>
2020-09-17 16:53:55 +0200

    tasklist p2

--- section4/tasklist/app.js
+++ section4/tasklist/app.js
@@ -1,9 +1,9 @@
 //Define ui vars
-const form = document.querySelector("#task-form");
-const taskList = document.querySelector(".collection");
-const clearBtn = document.querySelector(".clear-tasks");
-const filter = document.querySelector("#filter");
-const taskInput = document.querySelector("#task");
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
 
 //Load all event listeners
 loadEventListeners();
@@ -11,25 +11,31 @@ loadEventListeners();
 //Load all event listeners
 function loadEventListeners() {
   //ad task event
-  form.addEventListener("submit", addTask);
+  form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  //Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  //Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
 function addTask(e) {
-  if (taskInput.value === "") {
-    alert("add a task");
+  if (taskInput.value === '') {
+    alert('add a task');
   }
 
   // Create li element
-  const li = document.createElement("li");
+  const li = document.createElement('li');
   // Add a class
-  li.className = "collection-item";
+  li.className = 'collection-item';
   // create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
   // create  the delete link
-  const link = document.createElement("a");
+  const link = document.createElement('a');
   // Add class
-  link.className = "delete-item secondary-content";
+  link.className = 'delete-item secondary-content';
   // Add icon html
   link.innerHTML = '<i class="fa fa-remove">Delete</i>';
   // Append link to li
@@ -37,6 +43,38 @@ function addTask(e) {
   // append li to ul
   taskList.appendChild(li);
   // Clear input
-  taskInput.value = "";
+  taskInput.value = '';
   e.preventDefault();
+}
+
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+function clearTasks() {
+  //taskList.innerHTML = '';
+
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  console.log('target value', e.target.value);
+  document.querySelectorAll('.collection-item').forEach(function (task) {
+    const item = task.firstChild.textContent;
+    console.log('item', item);
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+      console.log('block');
+    } else {
+      task.style.display = 'none';
+      console.log('none');
+    }
+  });
 }
