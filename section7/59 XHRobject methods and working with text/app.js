5e1fc42c48f60c713d5c6c896815b35dabc6c907
Bart Klaasse <bart@bartklaasse.nl>
2020-09-20 15:53:36 +0200

    vid 59

---
+++ section7/59 XHRobject methods and working with text/app.js
@@ -0,0 +1,17 @@
+//Event listener for button
+document.getElementById('button').addEventListener('click', loadData);
+
+function loadData() {
+  //create an xhr object
+  const xhr = new XMLHttpRequest();
+  // Open request to path
+  xhr.open('GET', 'data.txt', true);
+  xhr.onload = function () {
+    if (this.status === 200) {
+      console.log(this.responseText);
+      document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
+    }
+  };
+
+  xhr.send();
+}
