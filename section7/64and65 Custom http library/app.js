3dc1fcb55149352a7084b3876932be74b8d92bab
Bart Klaasse <bart@bartklaasse.nl>
2020-09-27 18:35:15 +0200

    65 custom http library

--- section7/64and65 Custom http library/app.js
+++ section7/64and65 Custom http library/app.js
@@ -1,7 +1,49 @@
 const http = new easyHTTP();
 
-//Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function (error, callbackresponse) {
+// //Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function (error, callbackresponse) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(callbackresponse);
+//   }
+// });
+
+// //Get single post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (error, callbackresponse) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(callbackresponse);
+//   }
+// });
+
+//Create data to post
+const data = {
+  title: 'custom post',
+  body: 'this is a custom post blablabla',
+};
+
+// // Post Data to api
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (error, callbackresponse) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(callbackresponse);
+//   }
+// });
+
+// // Put data to api (update)
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (error, callbackresponse) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(callbackresponse);
+//   }
+// });
+
+//delete single post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (error, callbackresponse) {
   if (error) {
     console.log(error);
   } else {
--- section7/64and65 Custom http library/easyhttp.js
+++ section7/64and65 Custom http library/easyhttp.js
@@ -16,8 +16,42 @@ easyHTTP.prototype.get = function (url, callback) {
   };
   this.http.send();
 };
+
 // Post Request
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open('POST', url, true);
+  this.http.setRequestHeader('Content-Type', 'application/json');
+
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+  this.http.send(JSON.stringify(data));
+};
 
 // Put Request
+easyHTTP.prototype.put = function (url, data, callback) {
+  this.http.open('PUT', url, true);
+  this.http.setRequestHeader('Content-Type', 'application/json');
+
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+  this.http.send(JSON.stringify(data));
+};
 
 // Delete Request
+easyHTTP.prototype.delete = function (url, callback) {
+  this.http.open('DELETE', url, true);
+  let self = this;
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      //because this.http.status does not work due to function scoping, we assign the value of this to self and then we can get the status code
+      callback(null, 'Post deleted');
+    } else {
+      callback('Error:' + self.http.status);
+    }
+  };
+  this.http.send();
+};
