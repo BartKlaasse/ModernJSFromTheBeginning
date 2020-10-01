9d6d6a727354ccfc9ee08080e63b9ffa33b1fc85
Bart Klaasse <bart@bartklaasse.nl>
2020-10-01 18:12:52 +0200

    72 custom fetch async await

---
+++ section7/72 easyhttp3 fetch async await/app.js
@@ -0,0 +1,32 @@
+const http = new EasyHTTP();
+
+//Get users
+// const users = http
+//   .get('https://jsonplaceholder.typicode.com/users')
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+//User data
+const data = {
+  name: 'Harrold',
+  username: 'harry',
+  email: 'harry@harry.nl',
+};
+
+// // Create a post
+// const users = http
+//   .post('https://jsonplaceholder.typicode.com/users', data)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// update a user
+// const users = http
+//   .put('https://jsonplaceholder.typicode.com/users/2', data)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// Delete a user
+const users = http
+  .delete('https://jsonplaceholder.typicode.com/users/2')
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
---
+++ section7/72 easyhttp3 fetch async await/easyhttp3.js
@@ -0,0 +1,30 @@
+//EasyHttp v2
+
+class EasyHTTP {
+  // make a http get request
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+  // make a http post request
+  async post(url, data) {
+    const response = await fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) });
+    const resData = await response.json();
+    return resData;
+  }
+
+  // make a put request
+  async put(url, data) {
+    const response = await fetch(url, { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) });
+    const resData = await response.json();
+    return resData;
+  }
+
+  // make a delete request
+  async delete(url) {
+    const response = await fetch(url, { method: 'DELETE', headers: { 'Content-Type': 'application/json' } });
+    const resData = await 'Resource deleted';
+    return resData;
+  }
+}
