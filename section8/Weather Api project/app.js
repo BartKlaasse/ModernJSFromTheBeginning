43adcd469e4234d10d1c6627778f0852c5da18c2
Bart Klaasse <bart@bartklaasse.nl>
2020-10-04 17:22:25 +0200

    weatherjs ui

--- section8/Weather Api project/app.js
+++ section8/Weather Api project/app.js
@@ -1,5 +1,7 @@
 // Initialize Weather class
 const weather = new Weather('Den Helder', 'Nederland');
+//Init ui
+const ui = new UI();
 
 // Event listener: get weather on dom load
 document.addEventListener('DOMContentLoaded', getWeather);
@@ -7,6 +9,6 @@ document.addEventListener('DOMContentLoaded', getWeather);
 function getWeather() {
   weather
     .getWeather()
-    .then((result) => console.log(result))
+    .then((result) => ui.paint(result))
     .catch((err) => console.log(err));
 }
---
+++ section8/Weather Api project/ui.js
@@ -0,0 +1,24 @@
+class UI {
+  constructor() {
+    this.location = document.getElementById('w-location');
+    this.desc = document.getElementById('w-desc');
+    this.string = document.getElementById('w-string');
+    this.details = document.getElementById('w-details');
+    this.icon = document.getElementById('w-icon');
+    this.humidity = document.getElementById('w-humidity');
+    this.feelslike = document.getElementById('w-feels-like');
+    this.dewpoint = document.getElementById('w-dewpoint');
+    this.wind = document.getElementById('w-wind');
+  }
+
+  paint(weather) {
+    this.location.textContent = weather.name;
+    this.desc.textContent = weather.weather[0].description;
+    this.string.textContent = weather.main.temp + ' degrees celcius';
+    this.icon.setAttribute('src', `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    this.humidity.textContent = `Relative humidity ${weather.main.humidity}`;
+    this.feelslike.textContent = `Temp Feels like: ${weather.main.feels_like} degrees celcius`;
+    this.dewpoint.textContent = `Wind direction: ${weather.wind.deg} degrees`;
+    this.wind.textContent = `Wind speed: ${weather.wind.speed} meters/second`;
+  }
+}
